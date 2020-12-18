7155103a3a98d03ee6a80bfe5d91d33594c90e2c
Vinit Suryarao <suryarao.vinit@hotmail.com>
2020-12-18 10:05:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Single_Responsibility_Principle
+{
+    // S : Single Responsibility Principle (SRP)
+    // Each Class/Module should have single task.
+
+    interface IEmployee // Employee Login Task
+    {
+        void EmpLogin(string Id, string password);
+        void RegisterEmp(string EmpName);
+    }
+
+    interface ILogError // Error Log Task
+    {
+        void ErrorLog(string error);
+    }
+
+    interface ISendMail // Email Sent Task
+    {
+        void MailSend(string message);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
